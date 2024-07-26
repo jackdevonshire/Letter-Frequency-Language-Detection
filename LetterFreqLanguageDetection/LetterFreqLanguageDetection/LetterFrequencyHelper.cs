83cab779a7d776d51a67088cb02b10478ef8b576
Jack Devonshire <itunes.jd@hotmail.com>
2024-07-26 19:42:48 +0100

    Starting to add a helper to load in language letter frequencies

---
+++ LetterFreqLanguageDetection/LetterFreqLanguageDetection/LetterFrequencyHelper.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection;
+using Newtonsoft.Json;
+
+namespace LetterFreqLanguageDetection
+{
+    public class LetterFrequencyHelper
+    {
+        private LetterFrequencies _letterFrequencies;
+        public LetterFrequencyHelper(LetterFrequencies letterFrequencies)
+        {
+            _letterFrequencies = LoadLanguageFrequencies();
+        }
+
+        private LetterFrequencies LoadLanguageFrequencies()
+        {
+            var jsonFilePath = Path.Combine(Directory.GetParent(AppContext.BaseDirectory)!.Parent!.Parent!.Parent!.FullName, "frequencies.json");
+            var json = File.ReadAllText(jsonFilePath);
+            
+            return JsonConvert.DeserializeObject<LetterFrequencies>(json)!;
+        }
+
+        public LetterFrequencies GetLanguageLetterFrequencies()
+        {
+            return _letterFrequencies;
+        }
+
+        public Dictionary<char, double> GetLetterFrequenciesForText()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+public class LetterFrequencies
+{
+    public List<char> AllLetters { get; set; }
+    public List<LetterFrequenciesForLanguage> LanguageLetterFrequencies { get; set; }
+}
+public class LetterFrequenciesForLanguage
+{
+    public string LanguageName { get; set; }
+    public Dictionary<char, double> LetterFrequencies { get; set; }
+}
