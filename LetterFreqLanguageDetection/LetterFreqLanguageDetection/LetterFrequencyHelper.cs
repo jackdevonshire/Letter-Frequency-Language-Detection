f1646d004035abbc7438bf3911cb311315c0f5c1
Jack Devonshire <itunes.jd@hotmail.com>
2024-07-26 20:36:47 +0100

    Returning just a list of doubles for letter frequencies
    For the ChiSquaredAnalaysis we need the data points to be in the same order, which we can't really ensure when using the current dictionary structure. Now on loading the JSON, we order the available letters. We then use these ordered available letters when getting the frequencies for each language, and we also use this same order when getting frequencies for an input string to ensure the data points are always in the same index for the same character,

--- LetterFreqLanguageDetection/LetterFreqLanguageDetection/LetterFrequencyHelper.cs
+++ LetterFreqLanguageDetection/LetterFreqLanguageDetection/LetterFrequencyHelper.cs
@@ -16,7 +16,18 @@ namespace LetterFreqLanguageDetection
             var jsonFilePath = Path.Combine(Directory.GetParent(AppContext.BaseDirectory)!.Parent!.Parent!.Parent!.FullName, "frequencies.json");
             var json = File.ReadAllText(jsonFilePath);
             
-            return JsonConvert.DeserializeObject<LetterFrequencies>(json);
+            var letterFrequencies = JsonConvert.DeserializeObject<LetterFrequencies>(json)!;
+            letterFrequencies.AllLetters = letterFrequencies.AllLetters.Distinct().Order().ToList();
+            
+            foreach (var languageLetterFrequencies in letterFrequencies.LanguageLetterFrequencies)
+            {
+                foreach (var validLetter in letterFrequencies.AllLetters)
+                {
+                    languageLetterFrequencies.LetterFrequenciesRaw.Add(languageLetterFrequencies.LetterFrequencies[validLetter]);
+                }
+            }
+
+            return letterFrequencies;
         }
 
         public LetterFrequencies GetLanguageLetterFrequencies()
@@ -24,31 +35,31 @@ namespace LetterFreqLanguageDetection
             return _letterFrequencies;
         }
 
-        public Dictionary<char, double> GetLetterFrequenciesForText(string inputString)
+        public List<double> GetLetterFrequenciesForText(string inputString)
         {
-            var allowedCharacters = _letterFrequencies.AllLetters;
+            var orderedAllowedCharacters = _letterFrequencies.AllLetters;
             var letterCount = new Dictionary<char, int>();
-            foreach (var allowedCharacter in allowedCharacters)
+            foreach (var allowedCharacter in orderedAllowedCharacters)
             {
                 letterCount.Add(allowedCharacter, 0);
             }
 
-            var validCharacterCount = 0;
+            var validLettersInInputText = 0;
             foreach (var character in inputString)
             {
                 if (letterCount.ContainsKey(character))
                 {
                     letterCount[character]++; 
-                    validCharacterCount++;
+                    validLettersInInputText++;
                 }
             }
             
             // Now get the actual frequencies
-            var letterFrequencies = new Dictionary<char, double>();
-            foreach (var letter in letterCount)
+            var letterFrequencies = new List<double>();
+            foreach (var validLetter in orderedAllowedCharacters)
             {
-                double currentLetterFreq = ((double)letter.Value / (double)validCharacterCount) * 100;
-                letterFrequencies.Add(letter.Key, currentLetterFreq);
+                double currentLetterFreq = ((double)letterCount[validLetter] / (double)validLettersInInputText) * 100;
+                letterFrequencies.Add(currentLetterFreq);
             }
 
             return letterFrequencies;
@@ -63,6 +74,12 @@ public class LetterFrequencies
 }
 public class LetterFrequenciesForLanguage
 {
+    public LetterFrequenciesForLanguage()
+    {
+        LetterFrequenciesRaw = new List<double>();
+    }
+    
     public string LanguageName { get; set; }
     public Dictionary<char, double> LetterFrequencies { get; set; }
+    public List<double> LetterFrequenciesRaw { get; set; }
 }
